7292d57a8b68d4c1ac3daf7f8bc34310fde7a5ce
straseskiebellow554 <straseskiebellow554@gmail.com>
2025-03-05 20:04:12 +0000

    Add 1.go

---
+++ src/1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare a variable and initialize it to a value
+	var x int = 5
+
+	// Print the value of the variable to the console
+	fmt.Println(x)
+}
