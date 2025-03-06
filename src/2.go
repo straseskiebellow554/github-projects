1c184962529f1d39c81dd37946f187daca95adc9
straseskiebellow554 <straseskiebellow554@gmail.com>
2025-03-06 20:27:42 +0000

    Add 2.go

---
+++ src/2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Alice"
+	age := 30
+
+	if age > 21 {
+		fmt.Println("You can drink alcohol")
+	} else {
+		fmt.Println("You are not allowed to drink alcohol")
+	}
+}
