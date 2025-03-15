52444ded5b907272f0186c5c66483ab264ce9b23
straseskiebellow554 <straseskiebellow554@gmail.com>
2025-03-15 00:29:17 +0000

    Add 10.go

---
+++ src/10.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare a variable named `name` of type string
+	var name string
+
+	// Initialize the variable with a value
+	name = "Alice"
+
+	// Print the value of the variable to the console
+	fmt.Println("Hello, my name is", name)
+}
